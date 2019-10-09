2a3e5fe76b14dfc96b89c00d2d1a21ca011e1769
Patrick-Mondala <patrickmondala@gmail.com>
2019-10-08 21:18:21 -0700

    completed splash page

---
+++ frontend/components/session_form/signup_aside.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+    <aside className="signup-aside">
+        <h2>Connect with friends and the<br/>world around you on Bacefook.</h2>
+        <span><img src="https://scontent.fsac1-2.fna.fbcdn.net/v/t39.2365-6/32964924_188362901982436_5189181917978689536_n.png?_nc_cat=1&_nc_oc=AQkbqreQF-IRKfAbCBfD5b9i_DDlXr6StZ3PgpkgLKS9DlQ6nNJFNiJZxPfBEb36q6W_6ckgaE3flVFq2G3yTa_I&_nc_ht=scontent.fsac1-2.fna&oh=241fb6bdf2dbde5fc359a957b14e15fe&oe=5E356CA3" alt=""/><strong>See photos and update</strong> from friends in your News Feed.</span>
+        <span><img src="https://scontent.fsac1-2.fna.fbcdn.net/v/t39.2365-6/32967637_1400301410075884_2994305350271762432_n.png?_nc_cat=1&_nc_oc=AQnmE6MRyQDQb9oKs0zc9GNaYH91DBWKvpr-pY1aP6txyLiFRHeFpAn5eN6d27f19xI-tp8uNAI1-9z6SXF9AzbN&_nc_ht=scontent.fsac1-2.fna&oh=3a77c2a1934c32dc7bb11227158d95a8&oe=5E3859B0" alt=""/><strong>Share what's new</strong> in your life on your Timeline.</span>
+        <span><img src="https://scontent.fsac1-2.fna.fbcdn.net/v/t39.2365-6/33137320_1687624661315903_5362007326045765632_n.png?_nc_cat=1&_nc_oc=AQlvUr1S2sSuRHbg1huTJBUcvU84jeLHg5O1HgL4F85AxgHULQLYuZKI27V1Vazm14hgsEz-IVME60yWnNv7gghW&_nc_ht=scontent.fsac1-2.fna&oh=6b9342581ccf1fd9e3750a36e7ad0583&oe=5E296405" alt=""/><strong>Find more</strong> of what you're looking for Bacefook Search.</span>
+    </aside>
+);
