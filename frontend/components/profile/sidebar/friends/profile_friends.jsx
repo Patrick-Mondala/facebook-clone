105a6a449e40a0776a9535f3b6f6540e1050104e
Patrick-Mondala <patrickmondala@gmail.com>
2019-10-13 21:26:26 -0700

    added friends to sidebar nonfunctional

--- frontend/components/profile/sidebar/friends/profile_friends.jsx
+++ frontend/components/profile/sidebar/friends/profile_friends.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default class ProfileFriends extends React.Component {
     render() {
         return (
-            <div className="profile-">
-                <i class="fas fa-user-friends"></i>
+            <div className="profile-sidebar-profile-friends-container">
+                <p className="profile-sidebar-profile-friends-header">
+                    <span className="profile-sidebar-profile-friends-icon-container"><i className="fas fa-user-friends profile-sidebar-profile-friends-icon"></i></span>Friends
+                </p>
             </div>
         )
     }
--- frontend/components/profile/sidebar/profile_sidebar.jsx
+++ frontend/components/profile/sidebar/profile_sidebar.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ProfileIntro from './intro/profile_intro';
 import ProfilePhotos from './photos/profile_photos';
+import ProfileFriends from './friends/profile_friends';
 
 export default class ProfileSidebar extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="profile-sidebar">
                 <ProfileIntro user={this.props.user} currentUser={this.props.currentUser}/>
                 <ProfilePhotos user={this.props.user} currentUser={this.props.currentUser} />
+                <ProfileFriends user={this.props.user} currentUser={this.props.currentUser} />
             </div>
         )
     }
