a3603861e0b41788afbde376f96f1cfc492783e5
Patrick-Mondala <patrickmondala@gmail.com>
2019-10-15 14:35:16 -0700

    added cover picture button editing styling

--- frontend/components/profile/cover/cover_picture/cover_picture.jsx
+++ frontend/components/profile/cover/cover_picture/cover_picture.jsx
@@ -2,25 +2,49 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { openModal } from '../../../../actions/modal_actions';
 
-const ProfileCoverPicture = ({ user, currentUser, openModal }) => (
-    <div className="profile-cover-picture-container">
-        {user.id === currentUser.id ?
-            <div className="profile-cover-picture-edit-button-container">
-                <button
-                    onClick={openModal}
-                    className="profile-cover-picture-edit-button"
-                >
-                    <i id="profile-cover-profile-picture-edit-camera-icon" className="fas fa-camera"></i>
-                    {user.cover_picture ? "Edit Cover Photo" : "Add Cover Photo"}
-                </button>
-            </div> : null}
-        {user.cover_picture ?
-            (<img 
-                src={user.cover_picture}
-                className="profile-cover-picture"
-            />) : null}
-    </div>
-)
+class ProfileCoverPicture extends React.Component {    
+    constructor(props) {
+        super(props);
+        this.state = { hover: false }
+        this.enableHover = this.enableHover.bind(this);
+        this.disableHover = this.disableHover.bind(this);
+    }
+    
+    enableHover() {
+        this.setState({ hover: true })
+    }
+
+    disableHover() {
+        this.setState({ hover: false })
+    }
+    
+    render() {
+        return (
+            <div
+                onMouseOver={this.enableHover}
+                onMouseLeave={this.disableHover}
+                className="profile-cover-picture-container"
+            >
+                {this.props.user.id === this.props.currentUser.id ?
+                    (<div className="profile-cover-picture-edit-button-container">
+                        {this.state.hover || !this.props.user.cover_picture ? (<button
+                            onClick={this.props.openModal}
+                            className="profile-cover-picture-edit-button"
+                        >
+                            <i id="profile-cover-profile-picture-edit-camera-icon" className="fas fa-camera"></i>
+                            {this.props.user.cover_picture ? "Edit Cover Photo" : "Add Cover Photo"}
+                        </button>) :
+                    (<i id="profile-cover-profile-picture-edit-camera-icon-non-hover" className="fas fa-camera"></i>)}
+                    </div>) : null}
+                {this.props.user.cover_picture ?
+                    (<img 
+                        src={this.props.user.cover_picture}
+                        className="profile-cover-picture"
+                    />) : null}
+            </div>
+        )
+    }
+}
 
 const mapDispatchToProps = dispatch => ({
     openModal: () => dispatch(openModal('editCoverPicture'))
