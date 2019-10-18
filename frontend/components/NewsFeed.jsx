0302878984747559ff3495a9fbf5c08122108b3e
Patrick-Mondala <patrickmondala@gmail.com>
2019-10-18 04:35:47 -0700

    please work idk anymore

--- frontend/components/NewsFeed.jsx
+++ frontend/components/NewsFeed.jsx
@@ -26,7 +26,7 @@ class NewsFeed extends React.Component {
                 <ProfilePostForm user={this.props.currentUser} currentUser={this.props.currentUser} />
                 <div className="newsfeed-list-container">
                     <ul className="newsfeed-list">
-                    {this.props.posts.reverse().map(post => <ProfilePostIndexItem key={`newsfeed-post${post.id}`} timeline_owner_id={post.timeline_owner_id} author_id={post.author_id} post={post} />)}
+                    {this.state.posts.reverse().map(post => <ProfilePostIndexItem key={`newsfeed-post${post.id}`} timeline_owner_id={post.timeline_owner_id} author_id={post.author_id} post={post} />)}
                     </ul>
                 </div>
             </div>
