653eee5a77ad5fe162f11d8ebbb534f4ec3604e3
Patrick-Mondala <patrickmondala@gmail.com>
2019-10-18 04:44:28 -0700

    if it works, it works

--- frontend/components/NewsFeed.jsx
+++ frontend/components/NewsFeed.jsx
@@ -7,7 +7,6 @@ import ProfilePostForm from '../components/profile/timeline/profile_post_form';
 class NewsFeed extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {posts: this.props.posts}
     }
 
     componentDidMount() {
@@ -16,7 +15,7 @@ class NewsFeed extends React.Component {
 
     componentDidUpdate(prevProps) {
         if (!_.isEqual(prevProps.posts, this.props.posts)) {
-            this.props.fetchNewsfeed().then(() => this.setState({posts: this.props.posts}));
+            this.props.fetchNewsfeed();
         }
     }
 
@@ -26,7 +25,7 @@ class NewsFeed extends React.Component {
                 <ProfilePostForm user={this.props.currentUser} currentUser={this.props.currentUser} />
                 <div className="newsfeed-list-container">
                     <ul className="newsfeed-list">
-                    {this.state.posts.sort().reverse().map(post => <ProfilePostIndexItem key={`newsfeed-post${post.id}`} timeline_owner_id={post.timeline_owner_id} author_id={post.author_id} post={post} />)}
+                    {this.props.posts.reverse().map(post => <ProfilePostIndexItem key={`newsfeed-post${post.id}`} timeline_owner_id={post.timeline_owner_id} author_id={post.author_id} post={post} />)}
                     </ul>
                 </div>
             </div>
